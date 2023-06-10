fa3c9813dd8d8128616ceda639683c15e3ed6f49
Hatamleh <hatem.hatamleh@gmail.com>
2023-06-11 01:31:49 +0200

    the end

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -33,5 +33,7 @@ public class DriverFactory {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         return driver;
+
+        // The end of this course!
     }
 }
